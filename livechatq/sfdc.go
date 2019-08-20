e9147514d03adcd3141e2f48b959d42036a9eff2
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2019-08-20 13:14:42 +0800

    add requests for http module request simplification

--- livechatq/sfdc.go
+++ livechatq/sfdc.go
@@ -1,10 +1,21 @@
 package livechatq
 
 import (
-	"fmt"
 	"net/http"
-	"net/url"
+
+	"github.com/agila/livechat-server-go/util"
 )
+
+/*
+payload := map[string]string{
+	"grant_type":    "password",
+	"client_id":     config.clientID,
+	"client_secret": config.clientSecret,
+	"username":      config.basicAuthEmail,
+	"password":      config.basicAuthPassword,
+}
+jsonValue, _ := json.Marshal(payload)
+*/
 
 // SfdcConfig : SFDC App Configuration
 type SfdcConfig struct {
@@ -16,20 +27,23 @@ type SfdcConfig struct {
 	clientSecret      string
 }
 
-// QueryStringBuilder : Build query string in url values
-func queryStringBuilder(qs *url.Values, qm map[string]string) {
-	// Iterate query map
-	for key, value := range qm {
-		(*qs).Add(key, value)
+// NewSfdcConfig : Create SFDC Config Object
+func NewSfdcConfig(authorizerURL string, liveChatURL string,
+	basicAuthEmail string, basicAuthPassword, clientID string,
+	clientSecret string) SfdcConfig {
+	return SfdcConfig{
+		authorizerURL,
+		liveChatURL,
+		basicAuthEmail,
+		basicAuthPassword,
+		clientID,
+		clientSecret,
 	}
 }
 
 // Authorize SFDC Requests
 func authorize(config SfdcConfig) *http.Response {
 	URL := config.authorizerURL + "/services/oauth2/token"
-	req, _ := http.NewRequest("GET", URL, nil)
-
-	queryString := req.URL.Query()
 	qmap := map[string]string{
 		"grant_type":    "password",
 		"client_id":     config.clientID,
@@ -37,25 +51,11 @@ func authorize(config SfdcConfig) *http.Response {
 		"username":      config.basicAuthEmail,
 		"password":      config.basicAuthPassword,
 	}
-
-	queryStringBuilder(&queryString, qmap)
-	req.URL.RawQuery = queryString.Encode()
-	resp, err := http.Get(req.URL.String())
-	if err != nil {
-		fmt.Println("Authorize Fail")
-	}
+	resp := util.Get(URL, qmap, nil)
 	return resp
 }
 
 // CreateSession : create livechat session
-func CreateSession(config SfdcConfig) {
-	// URL := config.liveChatURL + "/chat/rest/System/SessionId"
-	// payload := map[string]string{
-	// 	"grant_type":    "password",
-	// 	"client_id":     config.clientID,
-	// 	"client_secret": config.clientSecret,
-	// 	"username":      config.basicAuthEmail,
-	// 	"password":      config.basicAuthPassword,
-	// }
-	// jsonValue, _ := json.Marshal(payload)
+func CreateSession(config SfdcConfig) *http.Response {
+	URL := config.liveChatURL + "/chat/rest/System/SessionId"
 }
---
+++ util/requests.go
@@ -0,0 +1,74 @@
+package util
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+// Transform Map to querystring
+func _queryStringBuilder(qs *url.Values, qm map[string]string) {
+	for key, value := range qm {
+		(*qs).Add(key, value)
+	}
+}
+
+// ExtractRespBody : extract response body and transform to map
+func ExtractRespBody(resp http.Response) string {
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Payload Transform Fail")
+	}
+	defer resp.Body.Close()
+	bodyString := string(bodyBytes)
+	return bodyString
+}
+
+// Get : HTTP Get request
+func Get(URL string, queryString map[string]string,
+	headers map[string]string) *http.Response {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", URL, nil)
+	if headers != nil {
+		for key, value := range headers {
+			req.Header.Add(key, value)
+		}
+	}
+
+	if queryString != nil {
+		for key, value := range queryString {
+			req.URL.Query().Add(key, value)
+		}
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("HTTP GET Failed")
+	}
+
+	return resp
+}
+
+// Post : HTTP Post request
+func Post(URL string, payload map[string]string,
+	headers map[string]string) *http.Response {
+	client := &http.Client{}
+
+	jsonPayload, _ := json.Marshal(payload)
+	req, _ := http.NewRequest("POST", URL, bytes.NewBuffer(jsonPayload))
+	if headers != nil {
+		for key, value := range headers {
+			req.Header.Add(key, value)
+		}
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("HTTP POST Failed")
+	}
+
+	return resp
+}
